74d47c5339fb5a349143d0b9f52f5552a35c1697
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2023-12-17 20:10:23 +0900

    フロント実装をしよう問題７

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ↑↑上の記述は削除しましょう↑↑
   root to: "messages#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
+  
