bf19ecbb6434ea44fdb2636b6c9833c041b6bf9e
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2023-12-29 18:21:19 +0900

    コード整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+=======
+  devise_for :users
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
+end
+>>>>>>> Stashed changes
